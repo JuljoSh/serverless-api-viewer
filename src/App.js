132adc88ba7f2a3aaa7d68d66f86d456b3dcd04f
JuljoSh <120031848+JuljoSh@users.noreply.github.com>
2022-12-17 15:34:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
 import './App.css';
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
-// A function to make requests to our Workers API using a query
-const getImages = async query => {
-  // The base URL for our API
-  const url = "https://my-worker.juljo.workers.dev/"
-
-  const resp = await fetch(url, {
-    // Send a POST request
-    method: "POST",
-    // With a JSON-stringified body containing the query from our input
-    body: JSON.stringify({ query }),
-    // Set the `Content-type` header so our API knows that the request
-    // is sending JSON
-    headers: { 'Content-type': 'application/json' }
-  })
-  return resp.json()
-}
+const ACCESS_KEY = '2NlASwtkygJ8s5FmT-mEjzhldJgZh61Yr0I726I2Ohk';
 
 function App() {
-  // Store the query that we'll search for in a simple useState hook
-  const [query, setQuery] = useState("")
-  // Store the array of images from the API in an array
-  const [images, setImages] = useState([])
-  // When the search button is clicked, make a request to the API
-  // and set the response from it as our images array
-  const search = async () => {
-    const results = await getImages(query)
-    setImages(results)
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function searchImages() {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `https://api.unsplash.com/search/photos?query=${query}&client_id=${ACCESS_KEY}`
+        );
+        const data = await response.json();
+        setImages(data.results);
+      } catch (error) {
+        setError(error);
+      }
+      setIsLoading(false);
+    }
+
+    if (query) {
+      searchImages();
+    }
+  }, [query]);
+
+  function handleChange(event) {
+    setQuery(event.target.value);
   }
 
-  // When input#query changes, set query to the value of the input 
-  const updateQuery = evt => setQuery(evt.target.value)
-
   return (
-    <div className="App">
-      <div className="form">
-    <h1>Website GANGOSH</h1>
-        <input id="query" type="text" onChange={updateQuery} placeholder="Search query" />
-        <button onClick={search}>Search</button>
-      </div>
-
-      {/* Map through the array of images and render a set of images */}
-      {images.map(image =>
-        <a key={image.id} href={image.link} target="_blank">
-          <img src={image.image} />
-        </a>
+    <div>
+      <form>
+        <label>
+          Search:
+          <input type="text" value={query} onChange={handleChange} />
+        </label>
+      </form>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>An error occurred: {error.message}</div>
+      ) : (
+        <div>
+          {images.map(image => (
+            <img src={image.urls.small} alt={image.alt_description} key={image.id} />
+          ))}
+        </div>
       )}
     </div>
   );
